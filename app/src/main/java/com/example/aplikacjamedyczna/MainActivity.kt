fefa36627c1aee0866862e659e790ac28663862b
Pszemko-PC <przemek.skrzecz@gmail.com>
2022-02-26 15:30:00 +0100

    Główny layout z logowaniem zrobiony

---
+++ app/src/main/java/com/example/aplikacjamedyczna/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.aplikacjamedyczna
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+    }
+
+
+}
