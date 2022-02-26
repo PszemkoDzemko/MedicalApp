c75534b2d87284847727b6b3d348adc4dba316f3
Pszemko-PC <przemek.skrzecz@gmail.com>
2022-02-26 16:37:14 +0100

    Linki do przejścia od rejestracji do logowania i odwrotnie

--- app/src/main/java/com/example/aplikacjamedyczna/MainActivity.kt
+++ app/src/main/java/com/example/aplikacjamedyczna/MainActivity.kt
@@ -2,14 +2,16 @@ package com.example.aplikacjamedyczna
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-
     }
+    val registerToLabel = findViewById<TextView>(R.id.toRegisterLabel)
 
 
 }
